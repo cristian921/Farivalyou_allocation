f72b971202585fdcd4853140f88aa0e196f43ea7
crtom <cr.tommasino@outlook.it>
2019-02-21 19:53:52 +0100

    Primo commit

--- Farivalyou_allocation/settings.py
+++ Farivalyou_allocation/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,6 +132,9 @@ STATICFILES_DIRS = (
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
