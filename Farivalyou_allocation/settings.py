b69ed962fb22f567b12b2713901b2d36c9d26857
crtom <cr.tommasino@outlook.it>
2019-02-22 08:42:10 +0100

    Primo commit

--- Farivalyou_allocation/settings.py
+++ Farivalyou_allocation/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
 )
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
